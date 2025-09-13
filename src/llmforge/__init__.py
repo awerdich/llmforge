67211cde8bf0122d6b75bc9384805c2a803ad7f5
Andreas Werdich <andreas_werdich@hms.harvard.edu>
2025-09-13 08:53:57 -0400

    added author information to package

--- src/llmforge/__init__.py
+++ src/llmforge/__init__.py
@@ -1,2 +1,8 @@
+from pathlib import Path
+
+# you can use os.path and open() as well
+__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
+__author__ = Path(__file__).parent.joinpath("AUTHOR").read_text()
+
 def main() -> None:
     print("Hello from llmforge!")
